9ae617b3eca9b2d395c738376ab6ae3f48fd3576
Gregor Martynus <gregor@martynus.net>
2020-01-15 15:03:07 -0800

    test: TypeScript version

---
+++ test/retry.test.ts
@@ -0,0 +1,265 @@
+import { TestOctokit } from "./octokit";
+
+describe("Automatic Retries", function() {
+  it("Should be possible to disable the plugin", async function() {
+    const octokit = new TestOctokit({ retry: { enabled: false } });
+
+    try {
+      await octokit.request("GET /route", {
+        request: {
+          responses: [
+            { status: 403, headers: {}, data: { message: "Did not retry" } },
+            { status: 200, headers: {}, data: { message: "Success!" } }
+          ],
+          retries: 1
+        }
+      });
+      throw new Error("Should not reach this point");
+    } catch (error) {
+      expect(error.status).toEqual(403);
+      expect(error.message).toEqual("Did not retry");
+    }
+
+    expect(octokit.__requestLog).toStrictEqual(["START GET /route"]);
+  });
+
+  it("Should retry once and pass", async function() {
+    const octokit = new TestOctokit();
+
+    const response = await octokit.request("GET /route", {
+      request: {
+        responses: [
+          { status: 403, headers: {}, data: { message: "Did not retry" } },
+          { status: 200, headers: {}, data: { message: "Success!" } }
+        ],
+        retries: 1
+      }
+    });
+
+    expect(response.status).toEqual(200);
+    expect(response.data).toStrictEqual({ message: "Success!" });
+    expect(octokit.__requestLog).toStrictEqual([
+      "START GET /route",
+      "START GET /route",
+      "END GET /route"
+    ]);
+
+    expect(
+      octokit.__requestTimings[1] - octokit.__requestTimings[0]
+    ).toBeLessThan(20);
+  });
+
+  it("Should retry twice and fail", async function() {
+    const octokit = new TestOctokit();
+
+    try {
+      await octokit.request("GET /route", {
+        request: {
+          responses: [
+            { status: 403, headers: {}, data: { message: "ONE" } },
+            { status: 403, headers: {}, data: { message: "TWO" } },
+            { status: 401, headers: {}, data: { message: "THREE" } }
+          ],
+          retries: 2
+        }
+      });
+      throw new Error("Should not reach this point");
+    } catch (error) {
+      expect(error.status).toEqual(401);
+      expect(error.message).toEqual("THREE");
+    }
+    expect(octokit.__requestLog).toStrictEqual([
+      "START GET /route",
+      "START GET /route",
+      "START GET /route"
+    ]);
+    expect(
+      octokit.__requestTimings[1] - octokit.__requestTimings[0]
+    ).toBeLessThan(20);
+    expect(
+      octokit.__requestTimings[2] - octokit.__requestTimings[1]
+    ).toBeLessThan(20);
+  });
+
+  it("Should retry after 2000ms", async function() {
+    const octokit = new TestOctokit({ retry: { retryAfterBaseValue: 50 } });
+
+    const response = await octokit.request("GET /route", {
+      request: {
+        responses: [
+          { status: 403, headers: {}, data: {} },
+          { status: 202, headers: {}, data: { message: "Yay!" } }
+        ],
+        retries: 1,
+        retryAfter: 2
+      }
+    });
+
+    expect(response.status).toEqual(202);
+    expect(response.data).toStrictEqual({ message: "Yay!" });
+    expect(octokit.__requestLog).toStrictEqual([
+      "START GET /route",
+      "START GET /route",
+      "END GET /route"
+    ]);
+    // 50ms * 2 === 100ms
+    const ms = octokit.__requestTimings[1] - octokit.__requestTimings[0];
+    expect(ms).toBeGreaterThan(80);
+    expect(ms).toBeLessThan(120);
+  });
+
+  it("Should allow end users to see the number of retries after a failure", async function() {
+    const octokit = new TestOctokit({ retry: { retryAfterBaseValue: 25 } });
+
+    try {
+      await octokit.request("GET /route", {
+        request: {
+          responses: [
+            { status: 403, headers: {}, data: { message: "Failed, one" } },
+            { status: 403, headers: {}, data: { message: "Failed, two" } },
+            { status: 403, headers: {}, data: { message: "Failed, three" } },
+            { status: 403, headers: {}, data: { message: "Failed, four" } }
+          ],
+          retries: 3
+        }
+      });
+      throw new Error("Should not reach this point");
+    } catch (error) {
+      expect(error.message).toEqual("Failed, four");
+      expect(error.request.request.retryCount).toEqual(4);
+    }
+
+    // null (0) retryAfter
+    expect(
+      octokit.__requestTimings[3] - octokit.__requestTimings[2]
+    ).toBeLessThan(20);
+  });
+
+  it("Should allow end users to request retries", async function() {
+    const octokit = new TestOctokit({ retry: { retryAfterBaseValue: 25 } });
+
+    const response = await octokit.request("GET /route", {
+      request: {
+        responses: [
+          { status: 403, headers: {}, data: { message: "Did not retry" } },
+          { status: 202, headers: {}, data: { message: "Yay!" } }
+        ],
+        retries: 1,
+        retryAfter: 1
+      }
+    });
+
+    expect(response.status).toEqual(202);
+    expect(response.data).toStrictEqual({ message: "Yay!" });
+    expect(octokit.__requestLog).toStrictEqual([
+      "START GET /route",
+      "START GET /route",
+      "END GET /route"
+    ]);
+
+    const ms = octokit.__requestTimings[1] - octokit.__requestTimings[0];
+    expect(ms).toBeLessThan(45);
+  });
+
+  it("Should trigger exponential retries on HTTP 500 errors", async function() {
+    const octokit = new TestOctokit({ retry: { retryAfterBaseValue: 50 } });
+
+    const res = await octokit.request("GET /route", {
+      request: {
+        responses: [
+          { status: 500, headers: {}, data: { message: "Did not retry, one" } },
+          { status: 500, headers: {}, data: { message: "Did not retry, two" } },
+          {
+            status: 500,
+            headers: {},
+            data: { message: "Did not retry, three" }
+          },
+          { status: 200, headers: {}, data: { message: "Success!" } }
+        ]
+      }
+    });
+
+    expect(res.status).toEqual(200);
+    expect(res.data).toStrictEqual({ message: "Success!" });
+    expect(octokit.__requestLog).toStrictEqual([
+      "START GET /route",
+      "START GET /route",
+      "START GET /route",
+      "START GET /route",
+      "END GET /route"
+    ]);
+
+    const ms0 = octokit.__requestTimings[1] - octokit.__requestTimings[0];
+    expect(ms0).toBeLessThan(70);
+    expect(ms0).toBeGreaterThan(30);
+
+    const ms1 = octokit.__requestTimings[2] - octokit.__requestTimings[1];
+    expect(ms1).toBeLessThan(220);
+    expect(ms1).toBeGreaterThan(180);
+
+    const ms2 = octokit.__requestTimings[3] - octokit.__requestTimings[2];
+    expect(ms2).toBeLessThan(470);
+    expect(ms2).toBeGreaterThan(420);
+  });
+
+  it("Should not retry 3xx/400/401/403/422 errors", async function() {
+    const octokit = new TestOctokit({ retry: { retryAfterBaseValue: 50 } });
+    let caught = 0;
+    const testStatuses = [304, 400, 401, 403, 404, 422];
+
+    for (const status of testStatuses) {
+      try {
+        await octokit.request("GET /route", {
+          request: {
+            responses: [
+              { status, headers: {}, data: { message: `Error ${status}` } },
+              { status: 500, headers: {}, data: { message: "Error 500" } }
+            ]
+          }
+        });
+      } catch (error) {
+        expect(error.message).toEqual(`Error ${status}`);
+        caught++;
+      }
+    }
+
+    expect(caught).toEqual(testStatuses.length);
+    expect(octokit.__requestLog).toStrictEqual(
+      testStatuses.map(x => "START GET /route")
+    );
+  });
+
+  it("Should allow to override the doNotRetry list", async function() {
+    const octokit = new TestOctokit({
+      retry: {
+        doNotRetry: [400],
+        retries: 1,
+        retryAfterBaseValue: 50
+      }
+    });
+    let caught = 0;
+    const testStatuses = [304, 400, 401, 403, 404];
+
+    for (const status of testStatuses) {
+      try {
+        await octokit.request("GET /route", {
+          request: {
+            responses: [
+              { status, headers: {}, data: { message: `Error ${status}` } },
+              { status: 500, headers: {}, data: { message: "Error 500" } }
+            ]
+          }
+        });
+      } catch (error) {
+        if (status === 400 || status < 400) {
+          expect(error.message).toEqual(`Error ${status}`);
+        } else {
+          expect(error.message).toEqual("Error 500");
+        }
+        caught++;
+      }
+    }
+
+    expect(caught).toEqual(testStatuses.length);
+  });
+});
---
+++ test/smoke.test.ts
@@ -0,0 +1,13 @@
+import { Octokit } from "@octokit/core";
+
+import { retry } from "../src";
+
+describe("Smoke test", () => {
+  it("is a function", () => {
+    expect(retry).toBeInstanceOf(Function);
+  });
+
+  it("retry.VERSION is set", () => {
+    expect(retry.VERSION).toEqual("0.0.0-development");
+  });
+});
