8a09b3cf0165adc1c8ebd980c5bf8176861e85b8
Michael B. Gale <mbg@github.com>
2026-02-18 12:24:12 +0000

    Define interfaces for the plugin, state, and options

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+import type { RequestError } from "@octokit/request-error";
+
+export interface RetryPlugin {
+  retry: {
+    retryRequest: (
+      error: RequestError,
+      retries: number,
+      retryAfter: number,
+    ) => RequestError;
+  };
+}
+
+export interface RetryState {
+  enabled: boolean;
+  retryAfterBaseValue: number;
+  doNotRetry: number[];
+  retries: number;
+}
+
+export interface RetryOptions {
+  retry: RetryOptions;
+}
