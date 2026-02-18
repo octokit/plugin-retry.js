b2f6eef7fd65759084e18191620ba96d541d601b
Michael B. Gale <mbg@github.com>
2026-02-18 12:29:58 +0000

    Type `retry`

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,39 @@
-import type { Octokit } from "@octokit/core";
+import type { Octokit, OctokitOptions } from "@octokit/core";
 import type { RequestError } from "@octokit/request-error";
 
 import { VERSION } from "./version.js";
 import { errorRequest } from "./error-request.js";
 import { wrapRequest } from "./wrap-request.js";
+import type { RetryOptions, RetryPlugin, RetryState } from "./types.js";
+import type { RequestRequestOptions } from "@octokit/types";
 export { VERSION } from "./version.js";
 
-export function retry(octokit: Octokit, octokitOptions: any) {
-  const state = Object.assign(
+export function retry(
+  octokit: Octokit,
+  octokitOptions: OctokitOptions & Partial<RetryOptions>,
+): RetryPlugin {
+  const state: RetryState = Object.assign(
     {
       enabled: true,
       retryAfterBaseValue: 1000,
       doNotRetry: [400, 401, 403, 404, 410, 422, 451],
       retries: 3,
-    },
+    } satisfies RetryState,
     octokitOptions.retry,
   );
 
   if (state.enabled) {
-    octokit.hook.error("request", errorRequest.bind(null, state, octokit));
-    octokit.hook.wrap("request", wrapRequest.bind(null, state, octokit));
+    // We expect this plugin to be installed when these hooks fire, and so assume that the
+    // `octokit` object will have been mutated to be at least of type `Octokit & RetryPlugin`
+    // when the handlers are called.
+    octokit.hook.error(
+      "request",
+      errorRequest.bind(null, state, octokit as Octokit & RetryPlugin),
+    );
+    octokit.hook.wrap(
+      "request",
+      wrapRequest.bind(null, state, octokit as Octokit & RetryPlugin),
+    );
   }
 
   return {
@@ -32,7 +46,7 @@ export function retry(octokit: Octokit, octokitOptions: any) {
         error.request.request = Object.assign({}, error.request.request, {
           retries: retries,
           retryAfter: retryAfter,
-        });
+        } satisfies RequestRequestOptions);
 
         return error;
       },
